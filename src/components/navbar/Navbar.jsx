0f22b75c8e452222d909a2596f3e07707f8c7b17
Omer4ruq <omerfaruq4@gmail.com>
2024-09-10 17:16:00 +0600

    navbar done

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import "./Navbar.css";
 
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <div className="nav">
+      <div>
+        Bro <span className="underline">Media</span> <span>Solution</span>
+      </div>
+      <div>
+        <ul>
+          <li className="underline">Home</li>
+          <li>About Us</li>
+          <li>Services</li>
+          <li>Blogs</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
